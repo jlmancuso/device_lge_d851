446be1ed132d66a19c4882ac847a9e515114713c
Jose Adolfo Galdamez <josegalre@pac-rom.com>
2016-04-27 18:11:42 -0600

    d851: update fingerprint and description from 30D
    Change-Id: If6bade4bcdc2916f83e2a1a8e6e782d78154b442
    
    Conflicts:
    	broken.mk

--- broken.mk
+++ broken.mk
@@ -12,8 +12,8 @@ PRODUCT_NAME := broken_d851
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
     PRODUCT_NAME="g3_tmo_us" \
-    BUILD_FINGERPRINT="lge/g3_tmo_us/g3:5.0.1/LRX21Y/15227151285fc:user/release-keys" \
-    PRIVATE_BUILD_DESC="g3_tmo_us-user 5.0.1 LRX21Y 15227151285fc release-keys"
+    BUILD_FINGERPRINT="lge/g3_tmo_us/g3:6.0/MRA58K/1609517159bca:user/release-keys" \
+    PRIVATE_BUILD_DESC="g3_tmo_us-user 6.0 MRA58K 1609517159bca release-keys"
 
 # Broken Device Maintainers
     PRODUCT_BUILD_PROP_OVERRIDES += \
