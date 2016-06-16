6affdc0b45d8352cb6f359c4d66c235a03a2a3f1
jlmancuso <jlmancuso1134@gmail.com>
2016-06-16 12:23:42 -0500

    cyanideM update

--- cyanide.mk
+++ cyanide.mk
@@ -2,12 +2,12 @@ PRODUCT_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 $(call inherit-product, device/lge/d851/full_d851.mk)
 
-$(call inherit-product, vendor/broken/config/common_full_phone.mk)
+$(call inherit-product, vendor/cyanide/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/broken/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/cyanide/config/nfc_enhanced.mk)
 
-PRODUCT_NAME := broken_d851
+PRODUCT_NAME := cyanide_d851
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
@@ -15,6 +15,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT="lge/g3_tmo_us/g3:6.0/MRA58K/1609517159bca:user/release-keys" \
     PRIVATE_BUILD_DESC="g3_tmo_us-user 6.0 MRA58K 1609517159bca release-keys"
 
-# Broken Device Maintainers
-    PRODUCT_BUILD_PROP_OVERRIDES += \
-  	DEVICE_MAINTAINERS="Joe (roguesyko) Repasy"
+
